a8a418cd5d83c85b781a0d7cb55cef86ec1c527f
Ullas B <ullas20112002@gmail.com>
2024-07-01 21:44:23 +0530

    java1 & java2 is modufied by developer

--- java1.java
+++ java1.java
@@ -1,8 +1,7 @@
 
 class java1 {
     public static void main(String args[]) {
-        System.out.println("Hello World by main by java1 ");
-        System.out.println("This is the change by java1");
-
+        System.out.println("Hello World by main by java1 by developer");
+        System.out.println("This is the change by java1 by developer");
     }
 }
