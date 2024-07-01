afefa5945b4e552d6ad8d869c01a1019d7e997da
Ullas B <ullas20112002@gmail.com>
2024-07-01 19:30:59 +0530

    java2.java file is added

---
+++ java2.java
@@ -0,0 +1,7 @@
+
+class java2 {
+    public static void main(String args[]) {
+        System.out.println("Hello World by main by java 2");
+        System.out.println("This is the change by java 2");
+    }
+}
