34cfdd048330d6b937028b956a284c430fbf5b0f
Ullas B <ullas20112002@gmail.com>
2024-07-01 20:14:24 +0530

    This is my commit after

--- java1.java
+++ java1.java
@@ -1,7 +1,8 @@
 
 class java1 {
     public static void main(String args[]) {
-        System.out.println("Hello World by main ");
-        System.out.println("This is the change");
+        System.out.println("Hello World by main by java1 ");
+        System.out.println("This is the change by java1");
+
     }
 }
